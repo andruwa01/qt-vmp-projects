01c81704d48036c0370d515545ede68265066e68
andruwa_01 <cheprasov.work@gmail.com>
2024-12-05 15:21:04 +0300

    working on interface

---
+++ qt-vmp-interface-src/qt-vmp-user-inteface/mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // ip mask setup
+    // ip address in right format
+    QString ipRange = "(([ 0]+)|([ 0]*[0-9] *)|([0-9][0-9] )|([ 0][0-9][0-9])|(1[0-9][0-9])|([2][0-4][0-9])|(25[0-5]))";
+    QRegularExpression ipStrRegexp("^" + ipRange
+                     + "\\." + ipRange
+                     + "\\." + ipRange
+                     + "\\." + ipRange + "$");
+    QRegularExpressionValidator* ipValidator = new QRegularExpressionValidator(ipStrRegexp, this);
+    ui->label_ip_mask->setValidator(ipValidator);
+
+    // port mask setup
+    // [0, ... , 65535]
+    QRegularExpression portStrRegexp("^([1-9]|[1-9][0-9]|[1-9][0-9]{2}|[1-9][0-9]{3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]|655[0-2][0-9]|6553[0-5])$");
+    QRegularExpressionValidator* portValidator = new QRegularExpressionValidator(portStrRegexp, this);
+    ui->label_port_mask->setValidator(portValidator);
+
+    // frequency setup
+    // [1500, ..., 3000] MHZ
+    QRegularExpression freqStrRegexp("^(1500|1[5-9][0-9]{2}|[2-9][0-9]{3}|[12][0-9]{4}|30000)$");
+    QRegularExpressionValidator* freqValidator = new QRegularExpressionValidator(freqStrRegexp, this);
+    ui->label_freq_mask->setValidator(freqValidator);
+
+    // checking frequency wrong values [0, ..., 1499]
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
