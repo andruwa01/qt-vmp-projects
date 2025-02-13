6283f2f973d7616b221e32e1e08e900bb262addd
andruwa_01 <cheprasov.work@gmail.com>
2025-02-13 10:40:57 +0300

    refactor: add lock() on command.isSent = true

--- qt-vmp-interface-src/qt-vmp-user-inteface/socketworker.cpp
+++ qt-vmp-interface-src/qt-vmp-user-inteface/socketworker.cpp
@@ -118,19 +118,23 @@ void SocketWorker::processCommandQueue()
 
     int socket_ctrl = clientVmp->getSocketCtrl();
 
+    std::unique_lock<std::mutex> ul(mutex);
+
     CommandInfo &currentCommand = commandQueue.front();
 
     if (!currentCommand.isSent)
     {
+        ul.unlock();
         clientVmp->sendCommand(currentCommand);
+        ul.lock();
         currentCommand.isSent = true;
     }
 
-    std::unique_lock<std::mutex> ul(mutex);
     if (FD_ISSET(socket_ctrl, &readfds))
     {
         ul.unlock();
         clientVmp->receiveRespFromCommand(currentCommand);
+        ul.lock();
         commandQueue.pop();
     }
 }
